6c8b09696f7e2a800d57cea8084ec4bc384ae4b9
LaurieSue <pink.bee.me@outlook.com>
2020-01-02 22:58:39 -0600

    need to look at this vresion and compare and keep or delete

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,12 +1,31 @@
 
-{
-alert("Hello, World!");
-}
+
+	window.alert("Hello,World! ");
+
+	document.write("Hello, World!
 	
+	var D = "This is a glorious day! ";
+	var D =D.fontcolor("cornflowerblue");
+	document.write(D);
+
 	
-	{
-		var String =  "Kiss me, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML =
-		result;
-	}
+	var L ="Hi! I'm LaurieSue"
+	window.alert(L);
+
+	
+	document.write("Here are some of my favorite quotes: \"...for God has not given us a spirit of fear and timidity; but of power, love, and self-discipline\"- 2Timothy 1:7, The Bible, "
+	+  "and \"Be yourself; everyone else is already taken.\""
+	+ "- Oscar Wilde ");
+ 
+	
+	var quotes ="Here are some of my favorite quotes: \"...for God has not given us a spirit of fear and timidity; but of power, love, and self-discipline\"- 2Timothy 1:7, The Bible, "
+	+  "and \"Be yourself; everyone else is already taken.\""
+	+ "- Oscar Wilde "
+	var quotes = quotes.fontcolor("lime");
+	document.write(quotes)
+
+	var Faves = "Favorite Songs", tNa1 ="Broken, Lund", tNa2 = "Blue On Black, Kenny Wayne Shepard Band", tNa3 = "Wake Up, Mad Season "
+	var tNa3 = tNa3.fontcolor("fuchsia");
+	document.write(tNa3);
+
+	document.write(4*5+3);
